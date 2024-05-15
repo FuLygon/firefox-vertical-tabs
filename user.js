530008f2d2045c7a85f8be78519c057aaac258a4
Thanh Thai <thanhthai@fulygon.com>
2024-05-15 14:12:32 +0700

    update: user.js
    Signed-off-by: Thanh Thai <thanhthai@fulygon.com>

--- user.js
+++ user.js
@@ -3,3 +3,4 @@ user_pref("general.smoothScroll.msdPhysics.enabled", true);
 user_pref("ui.key.menuAccessKeyFocuses", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("layout.css.backdrop-filter.enabled", true);
